0c7ed05929e1f30c2192d2f03c6d6067135a4aa7
Bart <bartolomej.kozorog@gmail.com>
2024-06-24 12:39:25 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+if [ ! -f ./wasm_exec.js ]; then
+    # This should work, but doesn't: https://github.com/actions/setup-go/issues/71
+    # cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+    curl https://raw.githubusercontent.com/golang/go/go1.22.3/misc/wasm/wasm_exec.js > wasm_exec.js
+fi
+
 CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -tags=no_cgo -o flow-cli.wasm main.go
