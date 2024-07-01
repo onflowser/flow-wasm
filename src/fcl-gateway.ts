4a34c16b7d56f33efd9f373a22054aead92e6319
Bart <bartolomej.kozorog@gmail.com>
2024-07-01 10:57:46 +0200

    init vite library

---
+++ src/fcl-gateway.ts
@@ -0,0 +1,38 @@
+import * as fcl from "@onflow/fcl";
+import * as types from "@onflow/typedefs";
+import {FclScopedConfig} from "./fcl-scoped-config";
+
+type NetworkId = "testnet" | "mainnet" | "previewnet"
+
+export class FclGateway {
+    private readonly scopedConfig: FclScopedConfig<NetworkId>;
+
+    constructor(private readonly network: NetworkId) {
+        this.scopedConfig = new FclScopedConfig<NetworkId>();
+
+        this.scopedConfig.setConfig("testnet", {
+            'flow.network': 'testnet',
+            'accessNode.api': 'https://rest-testnet.onflow.org',
+        });
+
+        this.scopedConfig.setConfig("mainnet", {
+            'flow.network': 'mainnet',
+            'accessNode.api': 'https://rest-mainnet.onflow.org',
+        });
+
+        this.scopedConfig.setConfig("previewnet", {
+            'flow.network': 'previewnet',
+            'accessNode.api': 'https://rest-previewnet.onflow.org',
+        });
+    }
+
+
+    async getAccount(address: string): Promise<types.Account> {
+        this.prepare();
+        return fcl.account(address);
+    }
+
+    private prepare() {
+        this.scopedConfig.useConfig(this.network);
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+/// <reference types="vitest" />
+import path from "path";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+type Format = "es" | "cjs";
+
+const fileNameLookup = new Map<Format, string>([
+  ["es", `${getPackageName()}.mjs`],
+  ["cjs", `${getPackageName()}.cjs`]
+])
+
+const formats = Array.from(fileNameLookup.keys());
+
+module.exports = defineConfig({
+  base: "./",
+  build: {
+    outDir: "./dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: (format) => {
+        const filename = fileNameLookup.get(format as Format);
+
+        if (!filename) {
+          throw new Error("Filename not found for format: " + format)
+        }
+
+        return filename;
+      },
+    },
+  },
+  test: {},
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@@", replacement: path.resolve(__dirname) },
+    ],
+  },
+});
