be389521fcf6c54b8047ae3fe68b8ad62f6dae2d
Bart <bartolomej.kozorog@gmail.com>
2024-07-01 11:26:19 +0200

    update to build go files to dist dir

---
+++ build-go.sh
@@ -0,0 +1,7 @@
+if [ ! -f ./dist/wasm_exec.js ]; then
+    # This should work, but doesn't: https://github.com/actions/setup-go/issues/71
+    # cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+    curl https://raw.githubusercontent.com/golang/go/go1.22.3/misc/wasm/wasm_exec.js > ./dist/wasm_exec.js
+fi
+
+CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -tags=no_cgo -o ./dist/flow.wasm main.go
